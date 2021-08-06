0e96a18c6ec811a4230e2a589bf91dcf089b6850
Saiyeux <867257431@qq.com>
2021-08-06 11:30:54 +0800

    client connection

--- src/JackLinClient.cpp
+++ src/JackLinClient.cpp
@@ -1,5 +1,8 @@
 #include "JackLinClient.h"
 #include "ui_JackLinClient.h"
+#include "JackTripThread.h"
+#include "JackTripThread.h"
+#include "NetKS.h"
 
 JackLinkClient::JackLinkClient(QWidget *parent) :
     QWidget(parent),
@@ -18,4 +21,16 @@ void JackLinkClient::on_pushButton_2_clicked()
     JackLink *jacklink = new JackLink();
     this->close();
     jacklink->show();
+}
+
+void JackLinkClient::on_pushButton_clicked()
+{
+    QString seripadd = ui->lineEdit_2->text();
+    int p = ui->lineEdit->text().toInt();
+    JackTrip jacktrip(JackTrip::CLIENT);
+    jacktrip.setAllPorts(p);
+    jacktrip.setPeerAddress(seripadd);
+    NetKS netks;
+    jacktrip.appendProcessPluginFromNetwork(&netks);
+
 }
